735ba7ca1d50abc1bf3122240f3feb044b38f842
Zuguang Gu <jokergoo@gmail.com>
2020-12-19 14:06:13 +0100

    adjust according to BiocCheck

--- R/app.R
+++ R/app.R
@@ -52,11 +52,11 @@ ht_shiny = function(ht_list, ..., html = NULL) {
 		if(missing(ht_list)) {
 			cat("No heatmap is provided, use random heatmap\n")
 		    m1 = matrix(rnorm(100), 10)
-		    colnames(m1) = rownames(m1) = paste0("a", 1:10)
+		    colnames(m1) = rownames(m1) = paste0("a", seq_len(10))
 		    ht1 = Heatmap(m1, row_km = 2, column_km = 2)
 
-		    m2 = matrix(sample(letters[1:10], 100, replace = TRUE), 10)
-		    colnames(m2) = rownames(m2) = paste0("b", 1:10)
+		    m2 = matrix(sample(letters[seq_len(10)], 100, replace = TRUE), 10)
+		    colnames(m2) = rownames(m2) = paste0("b", seq_len(10))
 		    ht2 = Heatmap(m2, heatmap_legend_param = list(at = sort(unique(as.vector(m2)))))
 		    ht_list = draw(ht1 + ht2)
 		} else if(inherits(ht_list, "InputHeatmap")) {
@@ -106,7 +106,7 @@ ht_shiny_example = function(which) {
 	examples = get_examples()
 	if(missing(which)) {
 		cat("There are following examples:\n\n")
-		title = sapply(examples, function(x) x$title)
+		title = vapply(examples, function(x) x$title, "")
 		for(i in seq_along(title)) {
 			lines = strwrap(title[i], width = getOption("width") - 5)
 			lines[1] = paste0(ifelse(nchar(i) == 1, " ", ""), i, ". ", lines[1])
@@ -156,7 +156,7 @@ ht_shiny_example = function(which) {
 			}
 		} else {
 			i = which(grepl("shinyApp\\(", code))
-			code2 = c(code[1:(i-1)], "", code[seq(i, length(code))])
+			code2 = c(code[seq_len(i-1)], "", code[seq(i, length(code))])
 			code_line = paste(code, collapse = '\n')
 			code_line = gsub("'", "\\\\'", code_line)
 			code_line = gsub("<", "&lt;", code_line)
