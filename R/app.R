817bafa2596cf287c0fe629e923917cc6ed60053
Zuguang Gu <jokergoo@gmail.com>
2021-08-05 23:27:10 +0200

    improved the saved code

--- R/app.R
+++ R/app.R
@@ -179,9 +179,37 @@ htShiny = function(ht_list = get_last_ht(), title = NULL,
 		dir.create(save, showWarnings = FALSE)
 
 		save(list = ls(), file = paste0(save, "/htShiny.RData"))
-		code = "Rscript -e \"load('htShiny.RData');library(shiny);suppressPackageStartupMessages(library(InteractiveComplexHeatmap));suppressPackageStartupMessages(library(ComplexHeatmap));ui = fluidPage(title,description,if(hline) hr() else NULL,InteractiveComplexHeatmapOutput(heatmap_id = heatmap_id, title1 = title1, title2 = title2,width1 = width1, height1 = height1, width2 = width2, height2 = height2, layout = layout, compact = compact,action = action, cursor = cursor, response = response, brush_opt = brush_opt, output_ui_float = output_ui_float), html);server = function(input, output, session) {makeInteractiveComplexHeatmap(input, output, session, ht_list, sub_heatmap_cell_fun = sub_heatmap_cell_fun, sub_heatmap_layer_fun = sub_heatmap_layer_fun)};cat('If the shiny app is not automatically opened in the browser, you can manually copy the following link and paste it to the browser.');print(shinyApp(ui, server))\""
-		writeLines(code, con = paste0(save, "/htShiny.sh"))
-		writeLines(code, con = paste0(save, "/htShiny.bat"))
+		code = "
+load('htShiny.RData')
+chooseCRANmirror(ind = 1)
+setRepositories(ind = 1:2)
+if(!requireNamespace('InteractiveComplexHeatmap', quietly = TRUE)) {
+	install.packages('InteractiveComplexHeatmap')
+}
+library(shiny)
+suppressPackageStartupMessages(library(InteractiveComplexHeatmap))
+suppressPackageStartupMessages(library(ComplexHeatmap))
+ui = fluidPage(
+	title,
+	description,
+	if(hline) hr() else NULL,
+	InteractiveComplexHeatmapOutput(heatmap_id = heatmap_id, title1 = title1, title2 = title2,
+		width1 = width1, height1 = height1, width2 = width2, height2 = height2, layout = layout, compact = compact,
+		action = action, cursor = cursor, response = response, brush_opt = brush_opt, output_ui_float = output_ui_float), 
+	html
+)
+
+server = function(input, output, session) {
+	makeInteractiveComplexHeatmap(input, output, session, ht_list, 
+		sub_heatmap_cell_fun = sub_heatmap_cell_fun, sub_heatmap_layer_fun = sub_heatmap_layer_fun)
+}
+
+cat('If the shiny app is not automatically opened in the browser, you can manually copy the following link and paste it to the browser.');
+print(shinyApp(ui, server))
+"		
+		writeLines(code, con = paste0(save, "/htShiny.R"))
+		writeLines("Rscript htShiny.R", con = paste0(save, "/htShiny.sh"))
+		writeLines("Rscript htShiny.R", con = paste0(save, "/htShiny.bat"))
 		return(invisible(NULL))
 	}
 
